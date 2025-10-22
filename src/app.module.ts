4875b3e67af203f45008850b6c2f6c84827c39ec
Paulo Marcelo <marcelomp97447@gmail.com>
2025-10-22 10:04:42 -0300

    style: format imports in AppModule for better readability

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { AuthModule } from './auth/auth.module';
 import configuration from '@config/configuration';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [configuration], isGlobal: true }), AuthModule],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
